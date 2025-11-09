1ddbd8e2492a13e81f0b977a8c51222971bbe82b
xm4yestik <oxygamerz64@gmail.com>
2025-11-09 16:28:00 +0000

    feat: modify scripts and add container running checker script

--- scripts/01-setup_server.sh
+++ scripts/01-setup_server.sh
@@ -7,43 +7,30 @@ if [ "$EUID" -ne 0 ]; then
 	exit 1
 fi
 
-echo "--- [1/8] Starting Server Setup (Update Repositories) ---"
+echo "--- [1/6] Starting Server Setup (Update Repositories) ---"
 apt-get update
 
-echo "--- [2/8] Installing Tailscale and OpenSSH Server ---"
+echo "--- [2/6] Installing Tailscale and OpenSSH Server ---"
 apt-get install -y openssh-server
 
-echo "--- [3/8] Installing Neovim and Development Runtimes (Go, Node, Java) ---"
-apt-get install -y build-essential
-apt-get install -y software-properties-common
-add-apt-repository ppa:neovim-ppa/stable -y
-apt-get update
-apt-get install -y neovim
-apt-get install -y golang nodejs npm default-jdk
-
-echo "--- [4/8] Applying System Tweaks (Disable Sleep on Lid Close) ---"
+echo "--- [3/6] Applying System Tweaks (Disable Sleep on Lid Close) ---"
 sed -i -e 's/^#HandleLidSwitch=.*/HandleLidSwitch=ignore/' -e 's/^HandleLidSwitch=.*/HandleLidSwitch=ignore/' /etc/systemd/logind.conf
 systemctl restart systemd-logind.service
 
-echo "--- [5/8] Installing Core Tools & Dimming Screen ---"
+echo "--- [4/6] Installing Core Tools & Dimming Screen ---"
 apt-get install -y curl nano brightnessctl
 
 if command -v brightnessctl &> /dev/null; then
 	brightnessctl set 5%
 fi
 
-echo "--- [6/8] Starting SSH Service ---"
+echo "--- [5/6] Starting SSH Service ---"
 systemctl enable --now ssh
 
-echo "--- [7/8] Setting Up JAVA_HOME for Groovy LS ---"
-echo "export JAVA_HOME='/usr/lib/jvm/default-java'" | tee -a /etc/environment
-source /etc/environment
-
-echo "--- [8/8] Installing and Connecting Tailscale ---"
+echo "--- [6/6] Installing and Connecting Tailscale ---"
 curl -fsSL https://tailscale.com/install.sh | sh
 tailscale up
 
 echo " "
 echo "✅ Server Setup Complete."
-echo "Neovim LSPs should now install successfully via Mason."
 echo "Please visit the URL displayed above in your browser to authorize this device on your Tailnet."
